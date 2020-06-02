fe6b0376fc853f22c335858e60e2a943f474277e
Calvin Chong <calvin.chong@innity.com>
2020-06-02 19:42:03 +0800

    fix test method name `ip2loc`

--- tests/test_ip2loc.py
+++ tests/test_ip2loc.py
@@ -5,8 +5,8 @@ import apps.ip2loc as ip2loc
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-def test_get_geoloc():
+def test_ip2loc():
     ip_addr = '99.203.80.145'
-    data = ip2loc.get_geoloc(ip_addr)
+    data = ip2loc.ip2loc(ip_addr)
 
     assert data['subdiv1_name'] == 'Maryland'
